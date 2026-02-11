dd3251d18e3faba573a650ac83105ba6ccc70bb4
Abdujabbor <mahamatsiddiqovandujabbor@gmail.com>
2026-02-11 11:42:53 +0500

    malumotlar togirlandi

--- src/ProductData.js
+++ src/ProductData.js
@@ -133,25 +133,25 @@ const productData = [
   },
   {
     id: 13,
-    title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
-    price: 599,
-    oldPrice: 998.33,
+    title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    price: 34.19,
+    oldPrice: 56.99,
     discount: "-40%",
-    rating: 4.4,
-    description: "21.5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon FreeSync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16:9. Color Supported - 16.7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_t.png"
+    rating: 2.6,
+    description: "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_t.png"
   },
   {
     id: 14,
-    title: "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultra Wide Screen QLED ",
-    price: 999.99,
-    oldPrice: 1666.65,
+    title: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    price: 17.97,
+    oldPrice: 29.95,
     discount: "-40%",
-    rating: 2.2,
-    description: "49 inch super ultra wide 32:9 curved gaming monitor with dual 27 inch screen side by side Quantum dot technology, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144Hz high refresh rate and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_t.png"
+    rating: 2.9,
+    description: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_t.png"
   },
   {
     id: 15,
